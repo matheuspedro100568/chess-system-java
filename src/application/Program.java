cb7db1be9b786b0c4260b8d2f8e4c052d66d564f
Matheus Pedro da Silva Sousa <47851260+matheuspedro100568@users.noreply.github.com>
2019-03-25 08:05:01 -0500

    Update Program.java

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,6 +18,7 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
+		//eai  boi?
 		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
